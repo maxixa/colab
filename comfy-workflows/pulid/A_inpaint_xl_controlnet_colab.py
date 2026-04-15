7199e0c0385410a66e1a0e3bb2d947a55aab381d
maxixa <59010137+maxixa@users.noreply.github.com>
2026-04-16 06:03:19 +0700

    Update A_inpaint_xl_controlnet_colab.py

--- comfy-workflows/pulid/A_inpaint_xl_controlnet_colab.py
+++ comfy-workflows/pulid/A_inpaint_xl_controlnet_colab.py
@@ -30,9 +30,9 @@ height=1140
 # ckpt_name="Colossus_Project_XL_12C.SafeTensors"
 # ckpt_name="ProteusV0.4-RunDiffusionPhoto.safetensors"
 # ckpt_name="RealVisXL_V4.0.safetensors"
-ckpt_name="NightVisionXL-9.safetensors"
+#ckpt_name="NightVisionXL-9.safetensors"
 #ckpt_name="LEOSAM-s-HelloWorld-XL.safetensors"
-#ckpt_name="Realism_Engine_SDXL_3.safetensors"
+ckpt_name="Realism_Engine_SDXL_3.safetensors"
 #ckpt_name=""
 #ckpt_name=""
 
